00af906556def150997bc1ef6fdb9122268381a9
williamskei97 <williamskei97@gmail.com>
2021-11-27 11:44:57 -0500

    Add project files.

---
+++ MVVMApp/Utils/ObservableObject.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+namespace MVVMApp.Utils
+{
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
