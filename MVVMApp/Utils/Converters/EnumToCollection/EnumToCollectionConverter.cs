f3c742b8e904d5632a3237b1a75020b2d79a1e58
williamskei97 <williamskei97@gmail.com>
2022-01-23 16:07:11 -0500

    Added enumtocollection converter implementation and supporting classes.

---
+++ MVVMApp/Utils/Converters/EnumToCollection/EnumToCollectionConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+using System.Collections.Generic;
+
+namespace MVVMApp.Utils.Converters.EnumToCollection
+{
+    [ValueConversion(typeof(Enum), typeof(IEnumerable<ValueDescription>))]
+    public class EnumToCollectionConverter : MarkupExtension, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return EnumHelper.GetAllValuesAndDescriptions(value.GetType());
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+}
---
+++ MVVMApp/Utils/Converters/EnumToCollection/ValueDescription.cs
@@ -0,0 +1,8 @@
+﻿namespace MVVMApp.Utils.Converters.EnumToCollection
+{
+    public class ValueDescription
+    {
+        public object Value { get; set; }
+        public object Description { get; set; }
+    }
+}
